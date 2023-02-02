d3b8392b1f048affa8659711ecec22615bc4c355
bachacode <85910843+bachacode@users.noreply.github.com>
2023-02-01 21:21:16 -0400

    basic connection to db and query

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controllers;
 
+use PDO;
 use Petite\Routing\Route;
 
 class IndexController extends Controller
@@ -10,6 +11,22 @@ class IndexController extends Controller
     #[Route('/')]
     public function index()
     {
+        try {
+            $db = new PDO(
+                'mysql:host=localhost;
+                dbname=phpetite_test', 
+                'root', 
+                ''
+            );
+            $query = 'SELECT * FROM users';
+            $stmt = $db->query($query);
+            $test = $stmt->fetchAll();
+            print_r($test);
+        } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage(), $e->getCode());
+        }
+        
+        var_dump($db);
         return $this->view('index', ['foo' => 'bar']);
     }
 }
--- public/index.php
+++ public/index.php
@@ -11,7 +11,6 @@ require dirname(__DIR__) . '/routes/web.php';
 define('VIEW_PATH', __DIR__ . '/../app/Views/');
 define('LAYOUT_PATH', __DIR__ . '/../app/Views/layouts/');
 
-
 try {
     $app->resolve();
 } catch (\Petite\Http\HttpNotFoundException $e) {
